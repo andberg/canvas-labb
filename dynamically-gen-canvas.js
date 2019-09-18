2630cdc1dc8674adf0e212abe884dc935aef3626
Andrea Berglund <andrea.berglund@guidelinegeo.com>
2019-09-18 15:04:35 +0200

    Labb with generating canvases after eachother to simlate a GPR trace

---
+++ dynamically-gen-canvas.js
@@ -0,0 +1,54 @@
+// The function gets called when the window is fully loaded
+
+window.onload = function() {
+  console.log('main');
+
+  const height = 200;
+  let idGen = 1;
+
+  const addDataButton = document.getElementById('addDataBtn');
+  addDataButton.addEventListener('click', addData);
+
+  function addData() {
+    setInterval(() => {
+      generateData();
+    }, 10);
+  }
+
+  function getId() {
+    idGen = idGen + 1;
+    return idGen;
+  }
+
+  function createCanvasAndReturnId() {
+    const id = getId();
+    const currentCanvas = document.createElement('canvas');
+    currentCanvas.height = height;
+    currentCanvas.width = 1;
+    currentCanvas.setAttribute('id', id);
+    document.body.appendChild(currentCanvas);
+    return id;
+  }
+
+  function generateData() {
+    const data = [];
+    for (let i = 0; i < height; i++) {
+      data.push(i / height);
+    }
+    drawLine(data);
+    console.log(data);
+  }
+
+  function drawLine(dataArray) {
+    const currentId = createCanvasAndReturnId();
+    const currentCanvas = document.getElementById(currentId);
+    console.log(currentCanvas.width);
+    const currentContext = currentCanvas.getContext('2d');
+
+    const x = 0;
+    for (var i = 0; i < dataArray.length; i++) {
+      currentContext.fillStyle = 'rgba(1,1,1,' + dataArray[i] + ')';
+      currentContext.fillRect(x, i, 1, 1);
+    }
+  }
+};
--- hard-coded-canvas-width.js
+++ hard-coded-canvas-width.js
@@ -4,38 +4,36 @@ window.onload = function() {
   console.log('main');
   const canvas = document.getElementById('viewport');
   const context = canvas.getContext('2d');
-  let width = canvas.width;
-  const height = canvas.height;
 
+  const height = canvas.height;
+  let nextXPos = 0;
   const addDataButton = document.getElementById('addDataBtn');
   addDataButton.addEventListener('click', addData);
 
   function addData() {
     function generateData() {
       const data = [];
-
       for (let i = 0; i < height; i++) {
         data.push(i / height);
       }
       drawLine(data);
       console.log(data);
     }
-
     generateData();
-    for (let i = 0; i < 10; i++) {}
   }
 
   function drawLine(dataArray) {
-    canvas.width = canvas.width + 1;
-    width = width + 1;
+    nextXPos = nextXPos + 1;
+    let x = nextXPos;
+
     for (var i = 0; i < dataArray.length; i++) {
-      console.log('x', width);
+      console.log('x', x);
       console.log('y', i);
       console.log('opacity', dataArray[i]);
 
       context.fillStyle = 'rgba(1,1,1,' + dataArray[i] + ')';
       console.log(context.fillStyle);
-      context.fillRect(width, i, 1, 1);
+      context.fillRect(x, i, 1, 1);
     }
   }
 };
